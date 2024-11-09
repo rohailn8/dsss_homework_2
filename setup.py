cd69ee523860d32ce7480a0a7ee8b266ebdc81ec
Rohail Naushad <RohailN8@gmail.com>
2024-11-09 18:33:21 +0100

    Added requirements.txt file containing information about the imported library, setup.py file defining setup steps and __init__.py file containing function imports

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',              
+    version='0.1.0',                
+    packages=find_packages(),              
+    author='Rohail Naushad',    
+    author_email='RohailN8@gmail.com',
+    description='Package for Math Quiz that tests users calculations against operations on 2 random integers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rohailn8/dsss_homework_2',  
+    classifiers=[                          
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',               
+)
